814486ba9d0ab66ca2f245efcebe7528ebd79f21
alicemariaarrica <alicemariaarrica@gmail.com>
2024-11-13 19:04:06 +0100

    AppMainFunction-commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import './App.css'
 
 
 import Card from "./components/Card/Card.jsx";
+
 export default function App() {
   return <Card title="Titolo" content="Contenuto" />;
 }
+
+{/*Esercizio
+Partendo dall'esercizio di ieri e dall'array di posts allegato, l'obiettivo è creare dinamicamente gli articoli all'interno della pagina.
+I dati relativi a ciascun post dovranno essere passati come props ai componenti.
+Ogni post ha una proprietà published, e gli articoli devono essere mostrati solo se questa proprietà è impostata su true.
+Assegnate colori diversi ai tag dei posts, ad esempio: verde per html, rosa per css, etc…*/}
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,9 +1,21 @@
-import posts from '../data/Array.js'
+import Posts from '../data/Array.js'
+
+import PostCard from "./Card/Card.jsx";
+
+
 
 export default function AppMain() {
+    // logica
+    const Published_Posts = posts.filter(post => post.published === true);
 
-
-
+    // markup
     return (
-    
+        <main>
+            <div className="container">
+                <div className="row">
+                    {Published_Posts.map(post => <Card key={post.id} post={post} />)}
+                </div>
+            </div>
+        </main>
+    )
 }
