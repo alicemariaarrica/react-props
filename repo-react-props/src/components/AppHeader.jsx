486c5f2846b65f714d45e70e812f68e0317fe3d9
alicemariaarrica <alicemariaarrica@gmail.com>
2024-11-13 15:38:12 +0100

    AppHeader.jsx-Array.js-commit-

---
+++ repo-react-props/src/components/AppHeader.jsx
@@ -0,0 +1,14 @@
+export default function appHeader() {
+
+    // logic
+    const title = "I miei post"
+
+    // markup (JSX)
+    return (
+        <header>
+            <div className="container">
+                <h1>{title}</h1>
+            </div>
+        </header>
+    )
+}
